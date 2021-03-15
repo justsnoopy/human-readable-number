8b0f9370855376769b1b422a3d91d7f66519e731
Viktor <imjustsnoopy@gmail.com>
2021-03-15 19:23:53 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let uniqueToTwenty = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let tenthToNinety = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let numbersArray = number.toString().split('');
+
+    if (number == 0) { return 'zero' }
+    if (number < 20) { return uniqueToTwenty[number] }
+    if (number < 100 && number > 19) {
+        return (tenthToNinety[numbersArray[0]] + ' ' + uniqueToTwenty[numbersArray[1]]).trim()
+    }
+    if (number > 99) {
+        return numbersArray[2] == '0' && numbersArray[1] == '0' ? (uniqueToTwenty[numbersArray[0]] + ' hundred ').trim() : (uniqueToTwenty[numbersArray[0]] + ' hundred ' + toReadable(Number(numbersArray[1] + numbersArray[2]))).trim();
+    }
 }
